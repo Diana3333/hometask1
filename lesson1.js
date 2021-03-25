973e8db5d6c67f8dd679e18ced28893379af64a6
Diana3333 <dianapat99@gmail.com>
2021-03-25 12:16:31 +0200

    11

---
+++ lesson1.js
@@ -0,0 +1,35 @@
+
+console.log("Patrii");
+//task3
+let data;
+data=5;
+let note;
+console.log(data);
+
+note=7;
+console.log(note)
+document.write("data");
+document.write("note");
+data=note;
+alert("data");
+
+//task4
+let name="Diana";
+console.log(typeof name)
+let age=21;
+console.log(typeof age);
+let sun=true;
+console.log(typeof sun);
+let degree;
+
+
+//task5
+let isAdmin = confirm('Do you have 18 years old?', 20);
+alert(isAdmin);
+console.log(typeof isAdmin)
+//task6
+let user={firstName:"Diana",
+lastName:"Patrii",
+group:576,
+age:22
+}
