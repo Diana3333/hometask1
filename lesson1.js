a78fc61d839ac9b37c5b41dd0a48920985c2160d
Diana3333 <dianapat99@gmail.com>
2021-03-25 17:30:27 +0200

    1111111

--- lesson1.js
+++ lesson1.js
@@ -1,33 +1,46 @@
 
+//task2
 console.log("Patrii");
 //task3
 let data;
 data=5;
 let note;
+note1=7;
 console.log(data);
-console.log(note)
-document.write("data");
-document.write("note");
-data=note;
-alert("data");
+console.log(note1)
+document.write(data);
+<br></br>
+document.write(note1);
+alert(data);
+alert(note1);
+data=note1;
+alert(data);
+
 
 //task4
-let name="Diana";
-console.log(typeof name)
+let name1="Diana";
+console.log(typeof name1)
 let age=21;
 console.log(typeof age);
 let sun=true;
 console.log(typeof sun);
 let degree;
+console.log(degree);
+let age1=null;
+console.log(age1);
 
 
 //task5
-let isAdmin = confirm('Do you have 18 years old?', 20);
-alert(isAdmin);
-console.log(typeof isAdmin)
+let isAdmin = confirm('Do you have 18 years old?');
+console.log( isAdmin)
 //task6
-let user={firstName:"Diana",
-lastName:"Patrii",
-group:576,
-age:22
-}
+let firstName="Diana";
+let LastName="Patrii";
+let age3=22;
+let group=576;
+document.write(firstName+LastName+age+group);
+//task 7
+let name4=prompt('what is your name',"Diana");
+let email=prompt('what is your email',"Diana576@gmail.com");
+let password=prompt('what is your name',"Diana333");
+alert("Dear "+name4+", your pasword is"+password+" ,your email is "+email);
