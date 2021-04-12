0c1e0e111d2717d43e632bf77f013900d3907f0b
Diana3333 <dianapat99@gmail.com>
2021-04-12 16:17:59 +0300

    khkh

---
+++ .vscode/hometask5/hometask5.js
@@ -0,0 +1,40 @@
+//task1
+ //task 1
+ 
+ function propsCount(){ 
+   let mentor = { 
+      course: "JS fundamental", 
+      duration: 3,
+      direction: "web-development" }
+    console.log(Object.keys(mentor).length);
+  ;
+}
+propsCount();
+//task2
+function ShowProps() {
+   let lyly = {
+   course:  5,
+   course1: 1,
+   course2: 2,
+   course3: 3,
+   course4: 4,
+   course7:function ShowProps1(){ return this.course+","+this.course1 +" ,"
+   +this.course2+ " ,"+ this.course3 +"," + this.course4}
+   
+}
+console.log(Object.keys(lyly));
+ console.log(lyly.course7());
+}
+ShowProps();
+lyly.course7();
+
+
+
+
+//task3
+function Student(firstName,lastName) {
+   this.firstName = firstName;
+   this.lastName=lastName;
+    function showFullName () {firstName+""+ lastName};
+
+}
