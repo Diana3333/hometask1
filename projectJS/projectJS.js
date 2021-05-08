7fca01620f02216e077176dab2433ea418551c52
Diana3333 <dianapat99@gmail.com>
2021-05-08 22:43:00 +0300

    ар

--- projectJS/projectJS.js
+++ projectJS/projectJS.js
@@ -1,7 +1,7 @@
 let householdAppliances =[
    {
    title:'Lamp',
-   image: src="lightOff.jpg",
+   image: src="https://cb.scene7.com/is/image/Crate/EllaWhiteTableLampOffSHF15",
 },
 {
    title:'blinds',
@@ -9,15 +9,27 @@ let householdAppliances =[
 },
 { title: 'air conditioning',
 image:'https://media-exp1.licdn.com/dms/image/C4D1BAQF_F8zt505Itg/company-background_10000/0/1553779056305?e=2159024400&v=beta&t=WqsX-YKtvV54nNBgZ5y8OFNqsVEYpG5jUtHuyaBJ_ns'}
-
+,
+{title:'television control',
+   image:'https://specials-images.forbesimg.com/imageserve/5e18ab57a854780006e8413a/960x0.jpg?fit=scale" '
+}
 
 ];
 for (const householdAppliance of householdAppliances){
    document.write(`
-   <div>
+   <div class="allItems">
    <h2> ${householdAppliance.title} </h2>
 
    <img src ="${householdAppliance.image}" alt"" >
 </div>
 `)
 }
+function changeColor(color) {
+   document.body. style.background = color;
+}
+function Items_on() {
+   changeColor('violet');    
+}     
+function Items_of() {
+   changeColor('white');    
+}     
